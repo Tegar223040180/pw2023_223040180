1879a4e3ac6faa3ec18ab004452d8f87050d07fe
Tegar223040180 <tegar.223040180@mail.unpas.ac.id>
2023-02-21 11:07:54 -0800

    tegar pertamakali upload

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,20 @@
+   <?php 
+   $nama = 'Tegar Samudra';
+   ?>
+   <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pendahuluan PHP</title>
+    </head>
+    <body>
+    
+    <h1>
+        <?php echo "Hello $nama!"; ?>
+    </h1>
+    <p><?php echo "Pemrograman web"; ?></p>
+    <p><?php echo 10+ "10"; ?></p>
+    </body>
+    </html>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,30 @@
+<?php 
+ $NRP = 80
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Aku adalah angka <strong> <?php echo $NRP ?></strong>
+    </p>
+    <p>
+        Jika aku dikali 5, maka aku sekarang mejadi <strong> <?php echo "$NRP" *5 ?> </strong>
+    </p>
+    <p>
+        Jika aku dibagi 2, maka aku sekarang menjadi <strong> <?php echo "$NRP" /2 ?> </strong>
+    </p>
+    <p>
+        Jika aku ditambah 75, maka aku sekarang menjadi <strong> <?php echo "$NRP" +75 ?> </strong>
+    </p>
+    <p>
+        Jika aku dikurang 20, maka aku sekarang menjadi <strong> <?php echo "$NRP" -20 ?> </strong>
+    </p>
+</body>
+</html>

8a327000d1f9cd974efe9b73d337e10e56cd7e70
Tegar223040180 <tegar.223040180@mail.unpas.ac.id>
2023-03-15 20:07:43 -0700

    menyelsaikan pertemuan 4

---
+++ kuliah/pertemuan4/latihan1.php
@@ -0,0 +1,14 @@
+<?php 
+
+function cekGanjilGenap($angka){
+    if ($angka % 2 == 0) {
+        echo "$angka adalah bilangan genap <br>";
+    } else {
+        echo "$angka adalah bilangan ganjil <br>";
+    }
+}
+ cekGanjilGenap(10);
+ cekGanjilGenap(5);
+ cekGanjilGenap(2034);
+
+?>
